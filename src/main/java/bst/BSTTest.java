8f965e20dc13ade38082b02507eb2ef8cd089e11
nur <ilasnrasyl@gmail.com>
2026-05-03 15:35:24 +0500

    Refactor BSTTest main method for clarity

--- src/main/java/bst/BSTTest.java
+++ src/main/java/bst/BSTTest.java
@@ -1,19 +1,14 @@
 package bst;
-
 public class BSTTest {
     public static void main(String[] args) {
-
-        BST<Integer, String> tree = new BST<>();
-
-        tree.put(5, "A");
-        tree.put(3, "B");
-        tree.put(7, "C");
-
-        for (var elem : tree) {
-            System.out.println("key is " + elem.getKey() +
-                    " and value is " + elem.getValue());
+        BST<Integer,String> tree=new BST<>();
+        tree.put(5,"A");
+        tree.put(3,"B");
+        tree.put(7,"C");
+        for (var elem:tree) {
+            System.out.println("key is "+elem.getKey()+
+                    " and value is "+elem.getValue());
         }
-
-        System.out.println("Size: " + tree.size());
+        System.out.println("Size: "+tree.size());
     }
 }
