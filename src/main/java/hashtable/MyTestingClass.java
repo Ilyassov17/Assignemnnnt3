63f75396cc9266196fc724335ef554f31798d51a
nur <ilasnrasyl@gmail.com>
2026-05-03 15:43:09 +0500

    Refactor MyTestingClass for formatting consistency

--- src/main/java/hashtable/MyTestingClass.java
+++ src/main/java/hashtable/MyTestingClass.java
@@ -1,27 +1,25 @@
 package hashtable;
-
 public class MyTestingClass {
     int id;
     String name;
-
-    public MyTestingClass(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public MyTestingClass(int id,String name) {
+        this.id=id;
+        this.name=name;
     }
-
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 31 * hash + id;
-        hash = 31 * hash + name.length();
+        int hash=7;
+        hash=31*hash+id;
+        hash=31*hash+name.length();
         return hash;
     }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof MyTestingClass)) return false;
-        MyTestingClass other = (MyTestingClass) o;
-        return id == other.id && name.equals(other.name);
+        if (this==o) 
+            return true;
+        if (!(o instanceof MyTestingClass)) 
+            return false;
+        MyTestingClass other=(MyTestingClass) o;
+        return id==other.id && name.equals(other.name);
     }
 }
