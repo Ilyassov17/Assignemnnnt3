086f9a680ed4cf6b35632a62ffe7f4cbe00b3ae9
nur <ilasnrasyl@gmail.com>
2026-05-03 15:41:46 +0500

    Update MyHashTable.java

--- src/main/java/hashtable/MyHashTable.java
+++ src/main/java/hashtable/MyHashTable.java
@@ -1,106 +1,86 @@
 package hashtable;
-
-public class MyHashTable<K, V> {
-
-    private static class HashNode<K, V> {
+public class MyHashTable<K,V> {
+    private static class HashNode<K,V> {
         K key;
         V value;
-        HashNode<K, V> next;
-
-        HashNode(K key, V value) {
-            this.key = key;
-            this.value = value;
+        HashNode<K,V> next;
+        HashNode(K key,V value) {
+            this.key=key;
+            this.value=value;
         }
     }
-
-    private HashNode<K, V>[] table;
+    private HashNode<K,V>[] table;
     private int capacity;
     private int size;
-
     @SuppressWarnings("unchecked")
     public MyHashTable(int capacity) {
-        this.capacity = capacity;
-        this.table = (HashNode<K, V>[]) new HashNode[capacity];
-        this.size = 0;
+        this.capacity=capacity;
+        this.table=(HashNode<K,V>[]) new HashNode[capacity];
+        this.size=0;
     }
-
     public MyHashTable() {
         this(101);
     }
-
     private int hash(K key) {
-        return Math.abs(key.hashCode()) % capacity;
+        return Math.abs(key.hashCode())%capacity;
     }
-
-    public void put(K key, V value) {
-        int index = hash(key);
-
-        HashNode<K, V> head = table[index];
-
-        while (head != null) {
+    public void put(K key,V value) {
+        int index=hash(key);
+        HashNode<K,V> head=table[index];
+        while (head!=null) {
             if (head.key.equals(key)) {
-                head.value = value;
+                head.value=value;
                 return;
             }
-            head = head.next;
+            head=head.next;
         }
-
-        HashNode<K, V> newNode = new HashNode<>(key, value);
-        newNode.next = table[index];
-        table[index] = newNode;
+        HashNode<K,V> newNode=new HashNode<>(key,value);
+        newNode.next=table[index];
+        table[index]=newNode;
         size++;
     }
-
     public V get(K key) {
-        int index = hash(key);
-        HashNode<K, V> head = table[index];
-
-        while (head != null) {
+        int index=hash(key);
+        HashNode<K,V> head=table[index];
+        while (head!=null) {
             if (head.key.equals(key)) {
                 return head.value;
             }
-            head = head.next;
+            head=head.next;
         }
         return null;
     }
-
     public V remove(K key) {
-        int index = hash(key);
-
-        HashNode<K, V> head = table[index];
-        HashNode<K, V> prev = null;
-
-        while (head != null) {
+        int index=hash(key);
+        HashNode<K,V> head=table[index];
+        HashNode<K,V> prev=null;
+        while (head!=null) {
             if (head.key.equals(key)) {
-                if (prev != null) {
-                    prev.next = head.next;
+                if (prev!=null) {
+                    prev.next=head.next;
                 } else {
-                    table[index] = head.next;
+                    table[index]=head.next;
                 }
                 size--;
                 return head.value;
             }
-            prev = head;
-            head = head.next;
+            prev=head;
+            head=head.next;
         }
         return null;
     }
-
     public int size() {
         return size;
     }
-
     public void printBuckets() {
-        for (int i = 0; i < capacity; i++) {
-            int count = 0;
-            HashNode<K, V> head = table[i];
-
-            while (head != null) {
+        for (int i=0;i<capacity;i++) {
+            int count=0;
+            HashNode<K,V> head=table[i];
+            while (head!=null) {
                 count++;
-                head = head.next;
+                head=head.next;
             }
-
-            System.out.println("Bucket " + i + ": " + count);
+            System.out.println("Bucket "+i+ ": "+count);
         }
     }
 }
